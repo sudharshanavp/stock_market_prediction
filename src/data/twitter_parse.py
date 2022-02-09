1d18fe74ececdff37e8a83b0d5f6a617be53ecf2
sudharshanavp <sudharshanavp@outlook.com>
2022-02-10 00:02:31 +0530

    feat: setting up tweepy for parsing from twitter

---
+++ src/data/twitter_parse.py
@@ -0,0 +1,9 @@
+import tweepy
+from src import Constants
+
+# Set Keys and Tokens for Authenticating API access
+auth = tweepy.OAuthHandler(Constants.API_CONSUMER_KEY, Constants.API_CONSUMER_SECRET)
+auth.set_access_token(Constants.API_ACCESS_TOKEN, Constants.API_ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+
