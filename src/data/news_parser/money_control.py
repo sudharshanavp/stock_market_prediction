f49024241b47a18068aeb4efe280de8cdbab0023
sudharshanavp <sudharshanavp@outlook.com>
2022-02-11 09:40:19 +0530

    feat: module to scrape moneycontrol news pages

---
+++ src/data/news_parser/money_control.py
@@ -0,0 +1,9 @@
+'''
+@File : money_control.py
+@Author : Sudharshana VP
+@Contact : sudharshanavp@protonmail.com
+'''
+
+import parse_utility
+
+URL_MONEY_CONTROL = "https://www.moneycontrol.com/news/tags/"
