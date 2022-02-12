c7a9d69cc28f38e7c7d42398531fa4617cd9cda4
sudharshanavp <sudharshanavp@outlook.com>
2022-02-13 00:32:14 +0530

    refactor: refactored the get_word_frequency function and formatted the code

--- src/data/parser_utils/parse_utility.py
+++ src/data/parser_utils/parse_utility.py
@@ -1,8 +1,8 @@
-'''
+"""
 @File : parse_utility.py
 @Author : Sudharshana VP
 @Contact : sudharshanavp@outlook.com
-'''
+"""
 
 import re
 from typing import List, NoReturn
@@ -13,7 +13,8 @@ import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-stopwords = stopwords.words('english')
+stopwords = stopwords.words("english")
+
 
 def get_page(url_string: str) -> BeautifulSoup:
     """Scrape the page specified by the input url
@@ -25,7 +26,8 @@ def get_page(url_string: str) -> BeautifulSoup:
         BeautifulSoup: return the page obtained
     """
     response = get(url_string)
-    return BeautifulSoup(response.text, 'html.parser')
+    return BeautifulSoup(response.text, "html.parser")
+
 
 def clean_article(article: str) -> List[str]:
     """Clean the article by removing punctuations and stop words
@@ -36,15 +38,35 @@ def clean_article(article: str) -> List[str]:
     Returns:
         List[str]: list of words in the article
     """
-    article = article.replace('/(\n)/gm', " ")
-    article = re.sub('[.,!?:;%&$^*@#)/(-''`"—=+]', ' ', article)
-    article = re.sub('[0-9]', ' ', article)
+    article = article.replace("/(\n)/gm", " ")
+    article = re.sub("[.,!?:;%&$^*@#)/(-" '`"—=+]', " ", article)
+    article = re.sub("[0-9]", " ", article)
     article = article.replace("`|’|”|“", "'")
     article = article.replace("/(\\x)/g", "")
-    new_stop_words = ["said", "also", "per", "cent", "would", "last", "first", "like", '\'', '\"', "\'", "\"", "’", "'s", "“", "”"]
+    new_stop_words = [
+        "said",
+        "also",
+        "per",
+        "cent",
+        "would",
+        "last",
+        "first",
+        "like",
+        "'",
+        '"',
+        "'",
+        '"',
+        "’",
+        "'s",
+        "“",
+        "”",
+    ]
     stopwords.extend(new_stop_words)
-    clean = [word for word in word_tokenize(article) if not word in stopwords]
+    clean = [
+        word for word in word_tokenize(article) if not word in stopwords  # noqa: E713
+    ]
     return clean
+
 
 def lower_case(article: List[str]) -> List[str]:
     """Convert all characters of the words in the article to lowercase
@@ -58,6 +80,7 @@ def lower_case(article: List[str]) -> List[str]:
     lower = [word.casefold() for word in article]
     return lower
 
+
 def sort_dictionary(dictionary: dict) -> dict:
     """Sort the input dictionary
 
@@ -67,8 +90,9 @@ def sort_dictionary(dictionary: dict) -> dict:
     Returns:
         dict: sorted dictionary
     """
-    sorted_dict = dict(sorted(dictionary.items(), key = lambda value: value[1]))
+    sorted_dict = dict(sorted(dictionary.items(), key=lambda value: value[1]))
     return dict(reversed(list(sorted_dict.items())))
+
 
 def export_as_csv(dictionary: dict) -> NoReturn:
     """Export dictionary as csv file
@@ -79,10 +103,11 @@ def export_as_csv(dictionary: dict) -> NoReturn:
     Returns:
         NoReturn: Export the csv file to data folder in project root directory
     """
-    export = {'word': list(dictionary.keys()), 'frequency': list(dictionary.values())}
+    export = {"word": list(dictionary.keys()), "frequency": list(dictionary.values())}
     export_dataframe = pd.DataFrame(export)
-    export_dataframe.to_csv('../../data/word_frequency.csv')
-    
+    export_dataframe.to_csv("../../data/word_frequency.csv")
+
+
 def update_frequency(element: str, dictionary: dict) -> dict:
     """Update frequency of each word in the dictionary
 
@@ -95,29 +120,31 @@ def update_frequency(element: str, dictionary: dict) -> dict:
     """
     if element in dictionary:
         dictionary[element] += 1
-    else: 
+    else:
         dictionary.update({element: 1})
     return dictionary
 
-def get_word_frequency(news_list: list) -> NoReturn:
+
+def get_word_frequency(article_list: List[str]) -> NoReturn:
     """Find the frequency of words in the article
 
     Args:
-        news_list (list): list of articles parsed from the webpage
+        article_list (List[str]): list of articles parsed from the webpage
 
     Returns:
         NoReturn: export word frequncy as a csv file
     """
-    for news in news_list:
-        news['article'] = news['article'].replace('/\r?\n|\r/g', " ")
-        
-    for news in news_list:
-        cleaned_article = clean_article(news['article'])
+    word_frequency = {}
+    for article in article_list:
+        article = article.replace("/\r?\n|\r/g", " ")
+
+    for article in article_list:
+        cleaned_article = clean_article(article["article"])
         cleaned_article = lower_case(cleaned_article)
-        
+
         for word in cleaned_article:
             if len(word) > 2:
                 word_frequency = update_frequency(word, word_frequency)
-    
+
     word_frequency_sorted = sort_dictionary(dict(word_frequency))
     export_as_csv(word_frequency_sorted)
