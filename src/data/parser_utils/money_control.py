4ae7db52a15303ffc7c6c63dbcb76f9992169bb9
sudharshanavp <sudharshanavp@outlook.com>
2022-06-13 23:34:42 +0530

    refactor: trying to fix import errors

--- src/data/parser_utils/money_control.py
+++ src/data/parser_utils/money_control.py
@@ -6,7 +6,7 @@
 
 from typing import List
 
-import parser_utils
+import parse_utility
 
 URL_MONEY_CONTROL = "https://www.moneycontrol.com/news/tags/"
 
@@ -22,7 +22,7 @@ def get_page_links(stock_name: str) -> List[str]:
     """
     base_url = URL_MONEY_CONTROL + stock_name.replace(" ", "-") + ".html"
     number_of_pages = int(
-        parser_utils.get_page(base_url).find_all("a", class_="last")[-1]["data-page"]
+        parse_utility.get_page(base_url).find_all("a", class_="last")[-1]["data-page"]
     )
     page_links = []
     for i in range(1, number_of_pages + 1):
@@ -34,7 +34,7 @@ def get_article_list(page_links: List[str]) -> List[str]:
     article_list = []
     for link in page_links:
         article_list.append(
-            parser_utils.get_page(link).find("ul", id="cagetory").find_all("a")
+            parse_utility.get_page(link).find("ul", id="cagetory").find_all("a")
         )
     return sum(article_list, [])
 
